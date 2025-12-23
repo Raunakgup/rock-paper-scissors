84e830635c6ab8f765c2eb7de01c8f330cd67b1c
Raunakgup <graunak424@gmail.com>
2025-12-23 19:06:55 +0530

    Implement restart functionality

--- javascript.js
+++ javascript.js
@@ -53,29 +53,66 @@ const humanScoreSpan = document.querySelector("#human-score");
 const computerScoreSpan = document.querySelector("#computer-score");
 const winOrLoseList = document.querySelector("#win-or-lose-list");
 const winnerAnnouncement = document.querySelector("#winner-announcement");
+const body = document.querySelector("body");
+const buttons = buttonContainer.querySelectorAll('button');
+let restartButton = null;
 
-function disableButtons(){
-    const buttons = buttonContainer.querySelectorAll('button');
+function enableButtons() {
     buttons.forEach(item => {
-        item.disabled=true;
-        item.classList.add("disabled");
+        item.disabled = false;
+        item.classList.remove("disabled");
     });
 }
-function handleButtonClick(e){
+function resetScores() {
+    scores.humanScore = 0;
+    scores.computerScore = 0;
+    humanScoreSpan.textContent = 0;
+    computerScoreSpan.textContent = 0;
+}
+function clearList(lst) {
+    while (lst.firstChild) {
+        lst.removeChild(lst.firstChild);
+    }
+}
+function clearPara(para) {
+    para.textContent = "";
+}
+function restartGame() {
+    enableButtons();
+    resetScores();
+    clearList(winOrLoseList);
+    clearPara(winnerAnnouncement);
+    restartButton.remove();
+    restartButton = null;
+}
+function addRestartOption() {
+    restartButton = document.createElement('button');
+    restartButton.textContent = "Restart";
+    body.appendChild(restartButton);
+    restartButton.addEventListener("click", restartGame);
+}
+function disableButtons() {
+    buttons.forEach(item => {
+        item.disabled = true;
+        item.classList.add("disabled");
+    });
+    addRestartOption();
+}
+function handleButtonClick(e) {
     const target = e.target;
     if (target.tagName !== "BUTTON") return;
 
-    const winOrLoseListElement = document.createElement('li'); 
-    winOrLoseListElement.textContent=playRound(target.id, getComputerChoice(), scores);
+    const winOrLoseListElement = document.createElement('li');
+    winOrLoseListElement.textContent = playRound(target.id, getComputerChoice(), scores);
     winOrLoseList.appendChild(winOrLoseListElement);
 
-    humanScoreSpan.textContent=scores.humanScore;
-    computerScoreSpan.textContent=scores.computerScore;
+    humanScoreSpan.textContent = scores.humanScore;
+    computerScoreSpan.textContent = scores.computerScore;
 
     if (scores.humanScore === 5) {
         winnerAnnouncement.textContent = "Congrats, You won!";
         disableButtons();
-    } 
+    }
     else if (scores.computerScore === 5) {
         winnerAnnouncement.textContent = "Oops, you lost!";
         disableButtons();
